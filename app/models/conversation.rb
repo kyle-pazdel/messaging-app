774cdf51b751719d07cc91bdba0633d70ed3428a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-10 11:33:49 -0700

    Adds associations to Conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,6 @@
 class Conversation < ApplicationRecord
+  belongs_to :sender, :foreign_key => :sender_id, class_name: "User"
+  belongs_to :recipient, :foregn_key => :recipient_id, class_name: "User"
+
+  has_many :messags, dependent: :destroy
 end
