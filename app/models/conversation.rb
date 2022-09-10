610dde99c58a00ebe21e394cd837028a868557b2
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-10 11:53:43 -0700

    Adds conversation validation and scope for existing conversation between users

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,4 +3,11 @@ class Conversation < ApplicationRecord
   belongs_to :recipient, :foregn_key => :recipient_id, class_name: "User"
 
   has_many :messags, dependent: :destroy
+
+  validates_uniqueness_of :sender_id, :scope => :recipient_id
+
+  scope :between -> (sender_id, recipient_id) do
+    where("(conversations.sender_id = ? AND conversations.recipient_id = ?) OR (conversations.sender_id = ? AND conversations.recipient.id = ?)", sender_id, recipient_id, recipient_id, sender_id)
+  end
+  
 end
