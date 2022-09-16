47fe54255ae04d01b6024b2f31b51abc33b439b7
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-16 13:28:21 -0700

    Adds test routes to http test file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :conversations do
-    resources :messages
-  end
   # Defines the root path route ("/")
   # root "articles#index"
 
